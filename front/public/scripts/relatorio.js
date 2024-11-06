a1873568707cfe73f3b4ae32dc0cf2e37b0c28bf
Valdsonmacedo <Valdsonmacedo15@gmail.com>
2024-11-06 19:47:16 -0300

    feat: Buscando rotas e imprimindo relatórios - finalizados

--- front/public/scripts/relatorio.js
+++ front/public/scripts/relatorio.js
@@ -64,7 +64,6 @@ VerificarToken(token, refresh)
 const token_decodificado = decodeToken(token)
 
 
-
 let novoTokenDeAcesso;
 let novoRefresh;
 
@@ -213,96 +212,99 @@ async function getTicketsRota(id_rota) {
 }
 
 
-async function criarTabelaRelatorio(dia) {
-
-    // const rotas = {
-    //     "23/10 (qua)": {
-    //         emitidos: 54,
-    //         confirmados: 42,
-    //         faltantes: 12,
-    //         semTicket: 10,
-    //         totalEmbarque: 52
-    //     },
-    //     "24/10 (qui)": {
-    //         emitidos: 38,
-    //         confirmados: 16,
-    //         faltantes: 22,
-    //         semTicket: 26,
-    //         totalEmbarque: 42
-    //     },
-    //     "25/10 (sex)": {
-    //         emitidos: 53,
-    //         confirmados: 45,
-    //         faltantes: 8,
-    //         semTicket: 4,
-    //         totalEmbarque: 49
-    //     }
-    // };
-
-
-    if (dia) {
-        console.log("data: ", dia)
-
-        var rotas = await listarRotas(dia)
-
-        if (rotas) {
-            var rotas_pesquisadas = rotas.filter((rota) => rota.data === dia)
-            
-        } else {
-            alert("Não há rotas armazenadas com essa data " + `"${dia}"`)
-        }
+async function criarTabelaRelatorio(data_inicial, data_final) {
+    // Verifica se o parâmetro "dia" foi passado
+    if (!data_inicial) {
+        alert("Por favor, forneça uma data para buscar as rotas.");
+        return;
     }
 
-    const tabelaBody = document.getElementById("tabela-body")
+    let rotas_pesquisadas = [];
+    try {
+
+        // Obtém as rotas para o dia solicitado
+        const rotas = await listarRotas(data_inicial, data_final);
+
+        rotas_pesquisadas = rotas
+
+        // Filtra as rotas para obter apenas aquelas que correspondem ao dia
+        if (!rotas && rotas.length < 0) {
+            
+            alert("Não há rotas armazenadas para o periodo: " + `"${data_inicial}-${data_final}"`);
+        }
+    } catch (error) {
+        alert("Erro interno ao obter rotas.");
+        console.error(error);
+    }
+
+    // Limpa o corpo da tabela
+    const tabelaBody = document.getElementById("tabela-body");
     tabelaBody.innerHTML = "";
 
-    let row = document.createElement('tr')
-    row.innerHTML = "Carregando dados..."
-    console.log(rotas_pesquisadas)
+    // Verifica se há rotas pesquisadas para exibir
+    for (const rota of rotas_pesquisadas) {
+        try {
+            const detalhesRota = await relatorio_rotas(rota.id); // Obtém dados específicos para cada rota
 
-    if (rotas_pesquisadas[dia]) {
-        const rota = rotas_pesquisadas[dia]
+            if (detalhesRota && detalhesRota.emitidos !== undefined) {
+                let row = document.createElement('tr');
+                row.innerHTML = `
+                    <td>${formatDate(detalhesRota.data) + "/" + detalhesRota.horario}</td>
+                    <td>${detalhesRota.emitidos}</td>
+                    <td>${detalhesRota.confirmados}</td>
+                    <td>${detalhesRota.faltantes}</td>
+                    <td>${detalhesRota.sem_ticket}</td>
+                    <td>${detalhesRota.total}</td>
+                    <td>
+                        <a href="../pages/relatorio_final.html?id=${rota.id}">
+                            Ver mais
+                        </a>
+                    </td>
+                `;
+                tabelaBody.appendChild(row);
+                
+            }  else {
+                // Exibe uma linha alternativa se detalhes da rota estiverem ausentes
+                let row = document.createElement('tr');
+                row.innerHTML = `
+                    <td colspan="7" style="text-align:center;">Dados da rota do periodo de ${data_inicial} a ${data_final} não disponíveis</td>
+                `;
+                tabelaBody.appendChild(row);
+            }
+
+        } catch (error) {
+            console.error("Erro ao obter detalhes da rota:", error);
+            let row = document.createElement('tr');
+            row.innerHTML = `
+                <td colspan="7" style="text-align:center;">Erro ao carregar detalhes para a rota de periodo de ${data_inicial} a ${data_final}!</td>
+            `;
+            tabelaBody.appendChild(row);
+        }
+    } 
+
+    if (rotas_pesquisadas.length === 0) {
+        // Se nenhuma rota for encontrada, exibe uma mensagem informativa
+        let row = document.createElement('tr');
         row.innerHTML = `
-            <td>${dia}</td>
-            <td>${rota.emitidos}</td>
-            <td>${rota.confirmados}</td>
-            <td>${rota.faltantes}</td>
-            <td>${rota.semTicket}</td>
-            <td>${rota.totalEmbarque}</td>
-            <td>
-                <a href="../pages/relatorio_final.html?id=${rota.id}">
-                    Ver mais
-                </a>
-            </td>
+            <td colspan="7" style="text-align:center;">Nenhuma rota encontrada para o periodo ${data_inicial} a ${data_final}!</td>
         `;
-        tabelaBody.appendChild(row)
-    } else {
-        row.innerHTML = `
-            <td colspan="7" style="text-align:center";>Nenhuma rota encontrada para o dia ${dia}</td>
-        `
         tabelaBody.appendChild(row);
     }
 }
 
 
-const filter_form = document.querySelector("#filter_form")
-filter_form.addEventListener("submit", function(event) {
+
+const filter_form = document.querySelector("#filter_form");
+
+filter_form.addEventListener("submit", async function(event) {
     event.preventDefault()
-    // criarTabelaRelatorio("24/10 (qui)")
+
     const data_inicial = document.getElementById("data_ini").value;
     const data_final = document.getElementById("data_fim").value;
-    console.log({data_inicial, data_final})
 
-    if (data_final === data_inicial) {
-        criarTabelaRelatorio(data_inicial)
-        
-    } else {
-        criarTabelaRelatorio(data_inicial, data_final)
-    }
+    await criarTabelaRelatorio(data_inicial, data_final)
+});
 
-    document.getElementById("data_ini").value = "";
-    document.getElementById("data_fim").value = "";
-})
 
 const gerarPDF = document.querySelector("#pdf")
 gerarPDF.addEventListener("click", function() {
@@ -319,16 +321,12 @@ gerarPDF.addEventListener("click", function() {
         const imgHeight = (canvas.height * imgWidth) / canvas.width; // Mantém a proporção da imagem
 
         pdf.addImage(imgData, 'PNG', 10, 30, imgWidth, imgHeight); // Ajusta a imagem centralizada
-        pdf.save("Tabela_Resumo_Embarques.pdf"); // Salva o PDF com o nome especificado
+        pdf.save(`Tabela_Resumo_Embarques.pdf`); // Salva o PDF com o nome especificado
     });
 })
 
-function relatorio_rotas(id) {
-    const url = url_base + `cortex/api/soticon/v1/rotas/relatorio_rotas/${id}`;
-
-
-    const loader = document.getElementById('loader');
-    loader.style.display = 'block'; 
+async function relatorio_rotas(id) {
+    const url = url_base + `cortex/api/soticon/v1/relatorio_rotas/${id}/`;
 
     const options = {
         method: "GET",
@@ -338,32 +336,23 @@ function relatorio_rotas(id) {
         }
     }
 
-    fetch(url, options)
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error("Erro ao carregar rotas do dia: " + response.status);
-            }
-            return response.json()
-        })
-        .then((data) => {
-            
-            return data.results
 
-        })
-        .catch ((error) => {
-
-        })
-        .finally(() => {
-            loader.style.display = "none"
-        })
+    try {
+        const response = await fetch(url, options);
+        if (!response.ok) {
+            throw new Error('Erro ao buscar relatório da rota');
+        }
+        const rotas = await response.json();
+        return rotas; // Certifique-se de que está retornando os dados aqui
+    } catch (error) {
+        console.error("Erro ao listar relatório da rota:", error);
+        return []; // Retorne um array vazio ou trate o erro de outra forma
+    }
 }
 
-async function listarRotas(dia) {
-    const url = url_base + `cortex/api/soticon/v1/rotas/?data=${formatDate(dia)}&status=executada`;
+async function listarRotas(data_inicial, data_final) {
 
-
-    // const loader = document.getElementById('loader');
-    // loader.style.display = 'block'; 
+    const url = url_base + `cortex/api/soticon/v1/rotas/?data_inicial=${data_inicial}&data_final=${data_final}&status=executada`;
 
     const options = {
         method: "GET",
@@ -373,21 +362,17 @@ async function listarRotas(dia) {
         }
     }
 
-    fetch(url, options)
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error("Erro ao carregar rotas do dia: " + response.status);
-            }
-            return response.json()
-        })
-        .then((data) => {
-
-            console.log(data)
-            return data.results
-        })
-        .catch ((error) => {
-            console.log(error)
-        })
+    try {
+        const response = await fetch(url, options);
+        if (!response.ok) {
+            throw new Error('Erro ao buscar rotas');
+        }
+        const rotas = await response.json();
+        return rotas.results; // Certifique-se de que está retornando os dados aqui
+    } catch (error) {
+        console.error("Erro ao listar rotas:", error);
+        return []; // Retorne um array vazio ou trate o erro de outra forma
+    }
 }
 
 
@@ -408,7 +393,7 @@ async function verificarTipoUsuario(id) {
             throw new Error("Erro ao puxar os usuários" + response.status);
         }
         const data = await response.json();
-
+        console.log(data)
         return data;
 
     } catch (error) {
